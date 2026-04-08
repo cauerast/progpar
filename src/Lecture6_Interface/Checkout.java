0162cec34d6b3a5c6f8b6cee823548d05857863d
cauerast <cauerast@gmail.com>
2026-04-07 22:14:35 -0300

    update

--- src/Lecture6_Interface/Checkout.java
+++ src/Lecture6_Interface/Checkout.java
@@ -1,7 +1,7 @@
 package Lecture6_Interface;
 
 public class Checkout {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Payment cc = new CreditCard();
         cc.authorize(700);
@@ -10,6 +10,11 @@ public class Checkout {
         Payment pix = new Pix();
         pix.authorize(500);
         pix.showPaymentInfo();
+
+        int []a = {1, 2, 4, 5, 6};
+
+        System.out.println(a);
+
 
     }
 }
