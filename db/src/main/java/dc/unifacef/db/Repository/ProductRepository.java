0568bd87defd05a8a2a2282108f897dc465053da
cauerast <cauerast@gmail.com>
2026-05-21 20:59:08 -0300

    update

--- db/src/main/java/dc/unifacef/db/Repository/ProductRepository.java
+++ db/src/main/java/dc/unifacef/db/Repository/ProductRepository.java
@@ -4,7 +4,6 @@ import dc.unifacef.db.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Long id(Long id);
     // that interface will contain a CRUD;
     // save(), findAll(), findById(), remove(), existsById(), count().
     // essa classe é a mais perto ao banco de dados do sistema
