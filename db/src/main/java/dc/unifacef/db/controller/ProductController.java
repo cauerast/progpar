e11399d31bc7390a369c19e65c000e98048e13de
cauerast <cauerast@gmail.com>
2026-05-28 18:01:26 -0300

    update product - db api in springboot

--- db/src/main/java/dc/unifacef/db/controller/ProductController.java
+++ db/src/main/java/dc/unifacef/db/controller/ProductController.java
@@ -3,14 +3,13 @@ package dc.unifacef.db.controller;
 import dc.unifacef.db.Service.ProductService;
 import dc.unifacef.db.model.Product;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController // recebe as requisicoes
-@RequestMapping("/products") // recebe somente as requisicoes de /products
+@RequestMapping("/product") // recebe somente as requisicoes de /products
 public class ProductController {
     // object from ProductService. dependency injection
     // we use a constructor for this
@@ -25,4 +24,29 @@ public class ProductController {
         return ResponseEntity.ok(service.list());
     }
 
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product){
+        Product newProduct = service.save(product);
+        URI uri = URI.create("/product/" + newProduct.getId());
+        return ResponseEntity.created(uri).body(newProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remove(@PathVariable Long id){
+        if(service.remove(id)){
+            return ResponseEntity.noContent().build(); // 204
+        } else {
+            return ResponseEntity.notFound().build(); // 404
+        }
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Product> patch(@PathVariable Long id, @RequestBody Product newProd){
+        Product response = service.patch(id, newProd);
+        if(response != null){
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
